96560da123632737e7719ba2dfaf5dd261a6dfc7
Romain DUBOC <romain@duboc.pro>
2021-07-24 12:17:43 +0200

    Add storage cache

--- src/accessory.ts
+++ src/accessory.ts
@@ -1,24 +1,18 @@
 import {
-  AccessoryConfig,
-  AccessoryPlugin,
-  API,
-  Characteristic,
-  CharacteristicEventTypes,
-  CharacteristicGetCallback,
-  CharacteristicSetCallback,
-  CharacteristicValue,
-  Formats,
-  HAP,
-  Logging,
-  Perms,
-  Service,
-  Units
-} from "homebridge";
+    AccessoryConfig,
+    AccessoryPlugin,
+    API,
+    CharacteristicValue,
+    HAP,
+    Logging,
+    Service,
+} from 'homebridge';
 
 /*
  * IMPORTANT NOTICE
  *
- * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module (or the "hap-nodejs" module).
+ * One thing you need to take care of is, that you never ever ever import anything directly from the "homebridge" module 
+ * (or the "hap-nodejs" module).
  * The above import block may seem like, that we do exactly that, but actually those imports are only used for types and interfaces
  * and will disappear once the code is compiled to Javascript.
  * In fact you can check that by running `npm run build` and opening the compiled Javascript file in the `dist` folder.
@@ -38,74 +32,80 @@ import {
  * like this for example and used to access all exported variables and classes from HAP-NodeJS.
  */
 let hap: HAP;
+let homebridge: API;
 
 /*
  * Initializer function called when the plugin is loaded.
  */
 export = (api: API) => {
-  hap = api.hap;
-  api.registerAccessory("States", States);
+    homebridge = api;
+    hap = api.hap;
+    api.registerAccessory('States', States);
 };
 
 class States implements AccessoryPlugin {
 
-  private readonly log: Logging;
-  private readonly name: string;
-  
-  private readonly service: Service;
-  private readonly informationService: Service;
-  private readonly stateServices: Service[] = [];
+    private readonly log: Logging;
+    private readonly name: string;
 
-  constructor(log: Logging, config: AccessoryConfig, api: API) {
-    this.log = log;
-    this.name = config.name;
+    private readonly service: Service;
+    private readonly informationService: Service;
+    private readonly stateServices: Service[] = [];
 
-    this.service = new hap.Service.StatefulProgrammableSwitch(this.name, 'state');
-    this.service.getCharacteristic(hap.Characteristic.ProgrammableSwitchOutputState)
-      .setProps({minValue: 0, maxValue: config.states.length-1});
+    private readonly storage;
 
-    this.service.getCharacteristic(hap.Characteristic.ProgrammableSwitchOutputState)
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
-        const i = value as number;
-        this.stateServices[i].getCharacteristic(hap.Characteristic.ProgrammableSwitchEvent).setValue(0);
-        callback();
-      });
+    constructor(log: Logging, config: AccessoryConfig) {
+        this.log = log;
+        this.name = config.name;
 
-    let i = 1;
-    for(const state of config.states) {
-      const s = new hap.Service.StatelessProgrammableSwitch(state, ""+i);
-      s.getCharacteristic(hap.Characteristic.ProgrammableSwitchEvent).setProps({minValue: 0, maxValue: 0});
-      s.addOptionalCharacteristic(hap.Characteristic.ServiceLabelIndex);
-      s.addOptionalCharacteristic(hap.Characteristic.Name);
-      s.getCharacteristic(hap.Characteristic.ServiceLabelIndex).setValue(i++);
-      s.getCharacteristic(hap.Characteristic.Name).setValue(state);
-      this.stateServices.push(s);
+        const dir = homebridge.user.persistPath();
+        this.storage = require('node-persist');
+        this.storage.initSync({ dir: dir, forgiveParseErrors: true });
+        const lastState = this.storage.getItemSync(this.name) || 0;
+
+        this.service = new hap.Service.StatefulProgrammableSwitch(this.name, 'state');
+        this.service.getCharacteristic(hap.Characteristic.ProgrammableSwitchOutputState)
+            .setProps({ minValue: 0, maxValue: config.states.length - 1 })
+            .onSet((value: CharacteristicValue) => {
+                const i = value as number;
+                this.storage.setItemSync(this.name, value);
+                this.stateServices[i].getCharacteristic(hap.Characteristic.ProgrammableSwitchEvent).setValue(0);
+            })
+            .value = lastState;
+
+        let i = 1;
+        for (const state of config.states) {
+            const s = new hap.Service.StatelessProgrammableSwitch(state, '' + i);
+            s.getCharacteristic(hap.Characteristic.ProgrammableSwitchEvent).setProps({ minValue: 0, maxValue: 0 });
+            s.addOptionalCharacteristic(hap.Characteristic.ServiceLabelIndex);
+            s.addOptionalCharacteristic(hap.Characteristic.Name);
+            s.getCharacteristic(hap.Characteristic.ServiceLabelIndex).setValue(i++);
+            s.getCharacteristic(hap.Characteristic.Name).setValue(state);
+            this.stateServices.push(s);
+        }
+
+        this.informationService = new hap.Service.AccessoryInformation()
+            .setCharacteristic(hap.Characteristic.Manufacturer, 'github.com/dubocr')
+            .setCharacteristic(hap.Characteristic.SerialNumber, '000-001')
+            .setCharacteristic(hap.Characteristic.Model, 'States');
     }
 
-    this.informationService = new hap.Service.AccessoryInformation()
-      .setCharacteristic(hap.Characteristic.Manufacturer, "github.com/dubocr")
-      .setCharacteristic(hap.Characteristic.SerialNumber, "000-001")
-      .setCharacteristic(hap.Characteristic.Model, "States");
-    
-    log.info("States initialized!");
-  }
+    /*
+     * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
+     * Typical this only ever happens at the pairing process.
+     */
+    identify(): void {
+        this.log('Identify!');
+    }
 
-  /*
-   * This method is optional to implement. It is called when HomeKit ask to identify the accessory.
-   * Typical this only ever happens at the pairing process.
-   */
-  identify(): void {
-    this.log("Identify!");
-  }
-
-  /*
-   * This method is called directly after creation of this instance.
-   * It should return all services which should be added to the accessory.
-   */
-  getServices(): Service[] {
-    return [
-      this.informationService,
-      this.service
-    ].concat(this.stateServices.map((service) => service));
-  }
+    /*
+     * This method is called directly after creation of this instance.
+     * It should return all services which should be added to the accessory.
+     */
+    getServices(): Service[] {
+        return [
+            this.informationService,
+            this.service,
+        ].concat(this.stateServices.map((service) => service));
+    }
 }
